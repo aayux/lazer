6d35c69dd0554bdead05a737a249cc90e56356b1
JackyCheng1999 <43019720+JackyCheng1999@users.noreply.github.com>
2025-04-12 17:06:00 -0500

    Update basil_p2_params.py

--- python/basil/basil_p2_params.py
+++ python/basil/basil_p2_params.py
@@ -20,13 +20,15 @@ dim   = (m + 2 * n + 11, 4 * n + 22)                    # dimension of A
 p = ceil(sqrt(2 * dim[1]) * tau ** 2 * sqrt(2 * dim[1] + 2 * dim[0]) + tau * sqrt(2 * dim[1] + 2 * dim[0]))
 
 wpart = [
-            list(range(0, 2 * n + 19)),                     # [ s e1 e2 ]
+            list(range(0, 16)),                             # [ s e1 ]
+            list(range(16, 2 * n + 19)),                    # e2
             list(range(2 * n + 19, 4 * n + 19)),            # [ h x ]
             [4 * n + 19, 4 * n + 20], [4 * n + 21]          # [ s1 s2 ], [ r ]
     ]
 
-wl2   = [ tau * sqrt(8 * deg), 0, sqrt(34034726), 0]    # l2-norm bounds
-wbin  = [ 0, 1, 0, 1 ]                                  # binary coeffs
+wl2   = [ tau * sqrt(8 * deg), tau * sqrt((2 * n + 3) * deg), 0, sqrt(34034726), 0]    # l2-norm bounds
+#wl2   = [ 0, 0, 0, 0, 0]    # l2-norm bounds
+wbin  = [ 0, 0, 1, 0, 1 ]                                  # binary coeffs
 #wrej  = [0, 0, 0, 1]                                   # rejection sampling: on r only
 
 # Optional: some linf-norm bound on w.
